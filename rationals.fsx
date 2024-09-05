62ce3ed53307d237f3b100e72b4fee50f5f1ed59
k0sm0sk <57615238+k0sm0sk@users.noreply.github.com>
2024-09-05 20:48:44 +0200

    create first doc

--- rationals.fsx
+++ rationals.fsx
@@ -1,19 +1,23 @@
 
 
-let qplus (a,b) (c,d) =
-	((a*d + b*c) , (b*d))
+let qplus (a,b) (c,d) = ((a*d + b*c) , (b*d)) ;;
 
-let minus (a,b) (c,d) = 
-	((a*d - b*c) , (b*d))
+let qplus2nd (a, b) (c, d) (e, f) = qplus (qplus (a, b) (c, d)) (e, f);;
 
-let qmult (a,b) (c,d) = 
-	((a*c) , (b*d))
+let qminus (a,b) (c,d) = ((a*d - b*c) , (b*d));;
 
-let qdiv (a,b) (c,d) =
-	((a*d) , (b*c))
+let qmult (a,b) (c,d) = ((a*c) , (b*d)) ;;
 
-let toString (a: int, b:int) = 
-	string a + "/" + string b
+let qdiv (a,b) (c,d) = ((a*d) , (b*c)) ;;
+
+let toString (a: int, b: int) = string a + "/" + string b ;;
+
+let qminus2nd (a, b) (c, d) (e, f) = qminus (a, b) (qmult (c, d) (e, f)) ;;
 
 // illustration
-printfn "1/2 + 1/3 = %s" (toString (qplus (1,2) (1,3)))
+printfn "1/2 + 1/3 = %s" (toString (qplus (1,2) (1,3))) ;;
+printfn "2/3 - 3/2 = %s" (toString (qminus (2,3) (3,2))) ;;
+printfn "1/4 + 3/4 + 1/2 = %s" (toString(qplus2nd (1,4) (3,4) (1,2))) ;;
+printfn "1/4 + 3/4 + 1/2 = %s" (toString(qminus2nd(3,4) (3,2) (1,2))) ;;
+
+
