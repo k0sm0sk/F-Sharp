d028091ae5ca7da9446cea6f3fada0ec86216ca4
k0sm0sk <daniel.friishasche@gmail.com>
2024-09-26 13:34:55 +0200

    Removal of surveyList

--- Code/Assignments/Assignment1/src/vector.fsx
+++ Code/Assignments/Assignment1/src/vector.fsx
@@ -2,12 +2,12 @@
 let vectorLength (a:float, b:float) = 
     sqrt(a**2 + b**2)
 
-printfn "\nThe length of the given vector is (%.3f) \n" (vectorLength (1, 1))
+printfn "\nThe length of vector 1 is (%.3f) \nThe length of vector 2 is (%.3f)\n" (vectorLength (1, 1)) (vectorLength (2, 4))
 
 let vectorAdd (a:float, b:float) (c:float, d:float) = 
     (a + c), (d + b)
 
-printfn "The two vectors added results in %A \n" (vectorAdd (10.0, 5.0) (-2.0, 8.0))
+printfn "Vector 1 & 2 added results in %A \nVector 3 & 4 added results in %A" (vectorAdd (10.0, 5.0) (-2.0, 8.0)) (vectorAdd(6.0, 8.0) (13.0,20.0))
 
 let vectorScale (a: float, b:float) (scaleValue:float) =
     (a * scaleValue), (b * scaleValue)
